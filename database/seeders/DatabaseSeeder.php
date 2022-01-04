8ce2a30d3baee0a3b1f01a434452d19cfac320cb
Legend Oscar <devlegendoscar@gmail.com>
2022-01-04 20:58:50 +0100

    Uncommented all seeder classes: would use the 'class' flag as selector

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,11 +17,11 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call('UsersTableSeeder');
         $this->call(CategoriesSeeder::class);
-        // $this->call(SubCategoriesSeeder::class);
-        // $this->call(StoresSeeder::class);
-        // $this->call(UserRolesSeeder::class);
-        // $this->call(UserSeeder::class);
-        // $this->call(LocationsSeeder::class);
+        $this->call(SubCategoriesSeeder::class);
+        $this->call(StoresSeeder::class);
+        $this->call(UserRolesSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(LocationsSeeder::class);
 
         // CategoryModel::factory()
         //     ->count(50)
